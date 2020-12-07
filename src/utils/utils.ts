a4395f8f12a474a5f364a368ee19e44625beba2e
juandxce <juandxce@gmail.com>
2020-12-06 21:50:42 -0600

    Changed sorter values to use UpperCase instead

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,7 @@
 export function sortArrayByProp(array: any[], prop: string) {
   return array.sort((a, b) => {
-    const val1 = a?.[prop].toLowerCase();
-    const val2 = b?.[prop].toLowerCase();
+    const val1 = a?.[prop].toUpperCase();
+    const val2 = b?.[prop].toUpperCase();
     if (val1 < val2) {
       return -1;
     } else if (val1 > val2) {
